3a417519b4f929cae5a75ed20de551f883c98e5e
bangedorrunt <icky-clench-revise@duck.com>
2025-11-23 12:33:07 +1100

    feat: disable tap dance and contextual key detection

--- bangedorrunt.c
+++ bangedorrunt.c
@@ -8,13 +8,11 @@ enum layers {
   NUM,
 };
 
-enum { TD_RSFT_LANG };
 enum custom_keycodes { LLOCK = SAFE_RANGE, USRNAME };
-// Short aliases for home row mods and other tap-hold keys.
+// short aliases for home row mods and other tap-hold keys.
 #define HYPR_ESC MT(MOD_HYPR, KC_ESC)
 
 #define NAV_W MT(MOD_MEH, KC_W)
-#define TD_RSFT TD(TD_RSFT_LANG)
 
 #define HRM_A LT(SYM, KC_A)
 #define HRM_S LALT_T(KC_S)
@@ -28,40 +26,31 @@ enum custom_keycodes { LLOCK = SAFE_RANGE, USRNAME };
 #define NUM_V LT(NUM, KC_V)
 
 ///////////////////////////////////////////////////////////////////////////////
-// Tap-hold configuration (https://docs.qmk.fm/tap_hold)
+// tap-hold configuration (https://docs.qmk.fm/tap_hold)
 ///////////////////////////////////////////////////////////////////////////////
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t* record) {
-  switch (keycode) {
-    case TD_RSFT:
-      return TAPPING_TERM - 35;  // Snappier hold for right Shift
-    default:
-      return TAPPING_TERM;
-  }
-}
-
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t* record) {
-  // If you quickly hold a tap-hold key after tapping it, the tap action is
-  // repeated. Key repeating is useful e.g. for Vim navigation keys, but can
-  // lead to missed triggers in fast typing. Here, returning 0 means we
+  // if you quickly hold a tap-hold key after tapping it, the tap action is
+  // repeated. key repeating is useful e.g. for vim navigation keys, but can
+  // lead to missed triggers in fast typing. here, returning 0 means we
   // instead want to "force hold" and disable key repeating.
   switch (keycode) {
-    // Repeating is useful for Vim navigation keys.
+    // repeating is useful for vim navigation keys.
     case HRM_J:
     case HRM_K:
     case KC_H:
     case HRM_L:
-      return QUICK_TAP_TERM;  // Enable key repeating.
+      return QUICK_TAP_TERM;  // enable key repeating.
     default:
-      return 0;  // Otherwise, force hold and disable key repeating.
+      return 0;  // otherwise, force hold and disable key repeating.
   }
 }
 
 #ifdef FLOW_TAP_TERM
 bool is_flow_tap_key(uint16_t keycode) {
   if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
-    return false;  // Disable Flow Tap on hotkeys.
+    return false;  // disable flow tap on hotkeys.
   }
-  // Extract the tap keycode from tap-hold keys
+  // extract the tap keycode from tap-hold keys
   uint16_t tap_keycode = keycode;
   if (IS_QK_MOD_TAP(keycode)) {
     tap_keycode = QK_MOD_TAP_GET_TAP_KEYCODE(keycode);
@@ -83,43 +72,30 @@ bool is_flow_tap_key(uint16_t keycode) {
 
 uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
                            uint16_t prev_keycode) {
-  // Only apply Flow Tap when following a letter key, and not hotkeys.
+  // only apply flow tap when following a letter key, and not hotkeys.
   if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
     switch (keycode) {
       case HRM_F:
       case HRM_J:
       case NAV_W:
       case NUM_V:
-        return FLOW_TAP_TERM - 35;  // Shorter timeout otherwise
+        return FLOW_TAP_TERM - 35;  // shorter timeout otherwise
       case HRM_A:
       case HRM_S:
       case HRM_D:
       case HRM_K:
       case HRM_L:
       case HRM_SCLN:
-        return FLOW_TAP_TERM;  // Longer timeout otherwise.
+        return FLOW_TAP_TERM;  // longer timeout otherwise.
     }
   }
 
-  return 0;  // Disable Flow Tap otherwise.
+  return 0;  // disable flow tap otherwise.
 }
 #endif  // FLOW_TAP_TERM
 
-// Track the previous keycode to make contextual decisions for speculative hold
-static uint16_t prev_keycode = 0;
-
 bool get_speculative_hold(uint16_t keycode, keyrecord_t* record) {
-  // Define keys that should have contextual speculative hold behavior
-  bool is_contextual_key = (keycode == HRM_F || keycode == HRM_J);
-  bool prev_was_space = prev_keycode == KC_SPC;
-
-  // Disable speculative hold for certain keys when they follow space
-  // to prevent issues with chorded sequences in editors and other applications
-  if (is_contextual_key && prev_was_space) {
-    return false;
-  }
-
-  switch (keycode) {  // Enable speculative holding for these keys.
+  switch (keycode) {  // enable speculative holding for these keys.
     case HRM_A:
     case HRM_S:
     case HRM_D:
@@ -132,27 +108,24 @@ bool get_speculative_hold(uint16_t keycode, keyrecord_t* record) {
     case NAV_W:
       return true;
   }
-  return false;  // Disable otherwise.
+  return false;  // disable otherwise.
 }
 
 #ifdef COMBO_ENABLE
 const uint16_t combo_dummy[] PROGMEM = {COMBO_END};
 combo_t key_combos[] = {
-    // Map dummy combo to KC_NO so it does nothing
+    // map dummy combo to KC_NO so it does nothing
     COMBO(combo_dummy, KC_NO),
 };
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
-// User macro callbacks (https://docs.qmk.fm/feature_macros)
+// user macro callbacks (https://docs.qmk.fm/feature_macros)
 ///////////////////////////////////////////////////////////////////////////////
 void keyboard_post_init_user(void) {}
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   if (record->event.pressed) {
-    // Update the previous keycode when a key is pressed
-    prev_keycode = keycode;
-
     switch (keycode) {
       case USRNAME:
         SEND_STRING_DELAY("bangedorrunt", TAP_CODE_DELAY);
@@ -163,7 +136,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   return true;
 }
 #ifdef KEY_OVERRIDE_ENABLE
-// Hyper + J/K/L/H -> Down/Up/Right/Left
+// hyper + j/k/l/h -> down/up/right/left
 const key_override_t hypr_j_override =
     ko_make_basic(MOD_MASK_CSAG, HRM_J, KC_DOWN);
 const key_override_t hypr_k_override =
@@ -177,8 +150,3 @@ const key_override_t* key_overrides[] = {&hypr_j_override, &hypr_k_override,
                                          &hypr_l_override, &hypr_h_override};
 #endif
 
-#ifdef TAP_DANCE_ENABLE
-// Double tap Shift to macOS input source switch (Ctrl+Space)
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_RSFT_LANG] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, LCTL(KC_SPC))};
-#endif
